bcd2e8b78e4b5041593e6bf7e8ee5b1042c35020
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-11-01 18:42:55 +0300

    feat: add employees controller and routes

---
+++ routes/employees.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const { add, all, remove, employee, edit } = require("../controllers/employees");
+const { auth } = require('../middleware/auth');
+
+// /api/employees
+router.get('/', auth, all);
+// /api/employees/:id
+router.get('/:id', auth, employee);
+// /api/employees/add
+router.post('/add', auth, add);
+// /api/employees/remove/:id
+router.post('/remove/:id', auth, remove);
+// /api/employees/edit/:id
+router.put('/edit/:id', auth, edit);
+
+module.exports = router;
+
+
+
+
