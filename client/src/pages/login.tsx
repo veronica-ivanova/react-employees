ac5dd4df39a5b5cb9a0d9b3aa4ca0cc533593bc7
veronica-ivanova <veronica.ivanova27@gmail.com>
2025-11-07 19:12:25 +0300

    feat(login): add login page with form validation

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,9 +1,30 @@
-import  { Layout }   from '../components/layout/layout'
- 
+import { CustomInput } from "../components/custom-input/custom-input";
+import { Layout } from "../components/layout/layout";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { PasswordInput } from "../components/password-input/password-input";
+import { CustomButton } from "../components/custom-button/custom-button";
+import { Link } from "react-router-dom";
+import { Paths } from "../paths"
+
 export const Login = () => {
   return (
     <Layout>
-        <div>Login</div>
+      <Row align="middle" justify="center">
+        <Card title="Войдите" style={{ width: "30rem" }}>
+          <Form onFinish={() => null}>
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <CustomButton type="primary" htmlType="submit">
+              Войти
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Нет аккаунта? <Link to={ Paths.register }>Зарегистрируйтесь</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
-  )
-}
+  );
+};
